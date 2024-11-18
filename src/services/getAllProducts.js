e8db8f2692bbda283486123df89ef7642d3170ef
FarahAkmaliaM <farahamey1@gmail.com>
2024-11-18 22:03:45 +0700

    upload

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,54 @@
+export default function getAllProducts() {
+  return [
+      {
+        "id": "ACC-BAG-001",
+        "slug": "bag-silver-fang",
+        "name": "Bag Silver Fang",
+        "category": "Accessories",
+        "price": 350000,
+        "stock": 20,
+        "imageUrl": "https://orebishopping.reactbd.com/static/media/bestSellerTwo.c2641a76c189fcc71670.webp",
+        "description": "A premium quality bag featuring a sleek Silver Fang design, durable and fashionable."
+      },
+      {
+        "id": "ACC-HAT-001",
+        "slug": "one-hat",
+        "name": "One Hat",
+        "category": "Accessories",
+        "price": 50000,
+        "stock": 100,
+        "imageUrl": "https://orebishopping.reactbd.com/static/media/spfOne.af2c5aa8d8015ffcccf4.webp",
+        "description": "A fun and stylish One hat for fans of the series."
+      },
+      {
+        "id": "AF-BON-001",
+        "slug": "boneka-kuru",
+        "name": "Boneka Kuru",
+        "category": "Figure",
+        "price": 150000,
+        "stock": 75,
+        "imageUrl": "https://orebishopping.reactbd.com/static/media/newArrFour.b8a5fda6839f2f0b1eaa.webp",
+        "description": "A cuddly and collectible Kuru doll, perfect for all ages."
+      },
+      {
+        "id": "AF-SXF-001",
+        "slug": "spy-x-family-figure",
+        "name": "Spy X Family Figure",
+        "category": "Figure",
+        "price": 350000,
+        "stock": 50,
+        "imageUrl": "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/17/4f7ba3f8-461a-4ecc-a1e3-3f9235db1803.jpg",
+        "description": "A cuddly and collectible, perfect for all ages."
+      },
+      {
+        "id": "AF-STL-001",
+        "slug": "pink-hairman",
+        "name": "Pink Hairman",
+        "category": "Tshirt",
+        "price": 80000,
+        "stock": 100,
+        "imageUrl": "https://nikfashions.in/wp-content/uploads/2023/08/Pink-Hair-Man-Anime-Oversize-T-shirt-1.jpg",
+        "description": "lorem ipsum"
+      }
+  ]
+}
